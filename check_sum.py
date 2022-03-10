2c3ace8bd35ea8ea3278cbbf9d61d1165d4a3614
aleena2008 <aleenaanngeorge@yahoo.com>
2022-03-10 20:29:05 +0530

    Check for given sum in Sorted Array
    Program that takes sorted array and sum as input to check whether the given sum can be acheived taking two numbers from the array

---
+++ check_sum.py
@@ -0,0 +1,18 @@
+def check_sum(arr, sum):
+    l = 0
+    r = len(arr)-1
+
+    while (l<r) and (l<len(arr)):
+        if arr[l]+arr[r]==sum:
+            print(arr[l], arr[r])
+            return
+        if arr[l]+arr[r]>sum:
+            r=r-1
+        else:
+            l+=1
+
+    return    
+
+arr = list(map(int,input("\nEnter the numbers : ").strip().split())) 
+sum = int(input("Sum: "))
+check_sum(arr, sum)
